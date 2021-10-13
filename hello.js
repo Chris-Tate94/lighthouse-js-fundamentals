498dc12a00edf389d0b1cad32f9c774b4c718cbd
Chris Tate <chris_tate_94@hotmail.com>
2021-10-13 15:19:43 +0000

    updated hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,20 @@
 const sayHello = function () {
-    console.log("Hello world")
+    console.log("Hello world");
 }
-sayHello()
+sayHello();
 
 const sayHello2 = function (name) {
-    console.log("hello " + name)
+    console.log("hello " + name);
 }
-sayHello2("Jon")
+sayHello2("Jon");
+
+const sayHelloToConsole = function (name) {
+    console.log("Hello " + name);
+} 
+sayHelloToConsole("Console");
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
